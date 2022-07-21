148ea1ce0c1d3f607e062ce1c27db2e438b7e2ef
Juan Manuel <jmanuelgrehuello@yahoo.com>
2022-07-21 15:32:23 -0300

    solucion nextauth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,9 +1,8 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { FirestoreAdapter } from "@next-auth/firebase-adapter";
-import { getApp, getApps, initializeApp } from "firebase/app";
+import { db } from "../../../firebase"
 
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -14,13 +13,5 @@ export default NextAuth({
         }),
         // ...add more providers here
     ],
-    adapter: FirestoreAdapter({
-        apiKey: "AIzaSyAS9GSPW3bG4gBHkAjcYmVa0pJ2tZ3F0gU",
-        authDomain: "disney-plus-jmg.firebaseapp.com",
-        projectId: "disney-plus-jmg",
-        storageBucket: "disney-plus-jmg.appspot.com",
-        messagingSenderId: "907302362126",
-        appId: "1:907302362126:web:b9f557b53a15651d77305f",
-        emulator: {},
-    }),
+    adapter: db,
 });
