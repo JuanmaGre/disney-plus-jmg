5e1399c3e4b7882b8493f3a420c7b2b6468bee03
Juan Manuel <jmanuelgrehuello@yahoo.com>
2022-07-21 12:08:07 -0300

    creación de componentes y elimino tsx

---
+++ firebase.js
@@ -0,0 +1,21 @@
+import { initializeApp, getApp, getApps } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAS9GSPW3bG4gBHkAjcYmVa0pJ2tZ3F0gU",
+    authDomain: "disney-plus-jmg.firebaseapp.com",
+    projectId: "disney-plus-jmg",
+    storageBucket: "disney-plus-jmg.appspot.com",
+    messagingSenderId: "907302362126",
+    appId: "1:907302362126:web:b9f557b53a15651d77305f"
+};
+
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
+const db = getFirestore()
+const storage = getStorage()
+
+
+export { app, db, storage }
---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+
+
+function Home () {
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-center py-2">
+          <Head>
+              <title>Create Next App</title>
+              <link rel="icon" href="/favicon.ico" />
+          </Head>
+
+        </div>
+    )
+}
+
+export default Home
