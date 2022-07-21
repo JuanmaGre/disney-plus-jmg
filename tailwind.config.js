f12cd9d231b29332e117563864fd346c374cb993
Juan Manuel <jmanuelgrehuello@yahoo.com>
2022-07-21 11:48:54 -0300

    installation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    mode: "jit",
+    purge: 
+        [
+            "./pages/**/*.{js,ts,jsx,tsx}", 
+            "./components/**/*.{js,ts,jsx,tsx}"
+        ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            backgroundImage: (theme) => ({
+                home: "url('/images/background.png')",
+            }),
+        },
+        fontFamily: {
+            body: ["Montserrat", "sans-serif"],
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [require("tailwind-scrollbar-hide")],
+};
